40b53aa9f51b6196114f5fe0e9e2a48eb1e4a544
Pioter17 <piotrwisnie@wp.pl>
2023-02-26 20:37:12 +0100

    some new components and first styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { OfferComponent } from './main-tabs/offer/offer.component';
+import { BasketComponent } from './main-tabs/basket/basket.component';
+import { ContactComponent } from './main-tabs/contact/contact.component';
+import { HomeComponent } from './main-tabs/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: "full"},
+  {path: 'home', component: HomeComponent},
+  {path: 'offer', component: OfferComponent},
+  {path: 'basket', component: BasketComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: "**", component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [HomeComponent,
+                                  OfferComponent,
+                                  BasketComponent,
+                                  ContactComponent,
+                                  PageNotFoundComponent];
